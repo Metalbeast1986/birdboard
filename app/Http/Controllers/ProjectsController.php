16c2a53c70c441f729b18a9d1fef49f5bef8a450
metalbeast1986 <ksistof1986@gmail.com>
2019-06-13 21:00:59 +0300

    Lesson_5

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -12,10 +12,32 @@ class ProjectsController extends Controller
         return view('projects.index', compact('projects'));
 
     }
+<<<<<<< Updated upstream
     public function store(){
 
         $attributes=request()->validate(['title'=>'required', 'description'=>'required']);
         Project::create($attributes);
+=======
+        
+    public function show(Project $project)
+    {
+    //    $project=Project::findOrFail(request('project'));
+        return view('projects.show', compact('project'));
+
+    } 
+    public function store()
+    {
+        $attributes=request()->validate([
+            'title'=>'required',
+            'description'=>'required'
+         
+        ]);
+
+       // $attributes['owner_id'] = auth()->id();
+
+        auth()->user()->projects()->create($attributes);
+  
+>>>>>>> Stashed changes
         return redirect('/projects');
 
     }
