e094f0719c1e290ef4312b11af69bb95770007b9
metalbeast1986 <ksistof1986@gmail.com>
2019-07-14 13:26:05 +0300

    Lesson_9

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,14 +2,18 @@
 
 @section('content')
     <div class="flex items-center mb-3">
-        <a href="/projects/create">Create New Project</a>
+        <a href="/projects/create">New Project</a>
     </div>
 
-    <ul>
-        @foreach($projects as $project)
-        <li>
-            <a href="{{ $project->path() }}"> {{$project->title}}</a>      
-        </li>
-        @endforeach
-    </ul>
+    <div class="flex">
+        @forelse($projects as $project)
+            <div class="bg-white mr-4 rounded shadow">
+                <h3>{{$project->title}}</h3>
+                
+                <div>{{$project->description}}</div>
+            </div>
+        @empty 
+            <div>No projects yet</div>    
+        @endforelse     
+    </div>
 @endsection
